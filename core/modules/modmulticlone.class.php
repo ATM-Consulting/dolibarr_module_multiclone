a19b08e177a057c70586b8fe003a9e38c6a46211
atm-florian.m <florian.mortgat@atm-consulting.fr>
2019-07-05 16:46:42 +0200

    spellcheck

--- core/modules/modmulticlone.class.php
+++ core/modules/modmulticlone.class.php
@@ -59,7 +59,7 @@ class modmulticlone extends DolibarrModules
 		// Module label (no space allowed), used if translation string 'ModuleXXXName' not found (where XXX is value of numeric property 'numero' of module)
 		$this->name = preg_replace('/^mod/i','',get_class($this));
 		// Module description, used if translation string 'ModuleXXXDesc' not found (where XXX is value of numeric property 'numero' of module)
-		$this->description = "Permettre de cloner en spécifiant le nombre de clone et la fréquence en mois";
+		$this->description = "Permettre de cloner en spécifiant le nombre de clones et la fréquence en mois";
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
 		$this->version = '1.0';
 		// Key used in llx_const table to save module status enabled/disabled (where MYMODULE is value of property name of module in uppercase)
