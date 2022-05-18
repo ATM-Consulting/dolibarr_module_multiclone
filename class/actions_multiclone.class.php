cfb93c4324847f36e53a7f278d17d4613a004ded
kevin <kevin.giuga@atm-consulting.fr>
2022-05-18 16:35:09 +0200

    Remove useless condition because already in setup

--- class/actions_multiclone.class.php
+++ class/actions_multiclone.class.php
@@ -69,8 +69,8 @@ class Actionsmulticlone
             || (in_array('invoicecard', explode(':', $parameters['context'])) && !empty($conf->global->MULTICLONE_ACTIVATE_FOR_INVOICE))
             || (in_array('invoicesuppliercard', explode(':', $parameters['context'])) && !empty($conf->global->MULTICLONE_ACTIVATE_FOR_SUPPLIER_INVOICE))
             || (in_array('propalcard', explode(':', $parameters['context'])) && !empty($conf->global->MULTICLONE_ACTIVATE_FOR_PROPAL))
-            || (in_array('salarycard', explode(':', $parameters['context'])) && !empty($conf->global->MULTICLONE_ACTIVATE_FOR_SALARY) && floatval(DOL_VERSION) >= 16.0)
-            || (in_array('socialecard', explode(':', $parameters['context'])) && !empty($conf->global->MULTICLONE_ACTIVATE_FOR_TAX) && floatval(DOL_VERSION) >= 16.0)) {
+            || (in_array('salarycard', explode(':', $parameters['context'])) && !empty($conf->global->MULTICLONE_ACTIVATE_FOR_SALARY))
+            || (in_array('socialecard', explode(':', $parameters['context'])) && !empty($conf->global->MULTICLONE_ACTIVATE_FOR_TAX))) {
             // Passage à l'action multiclone dès lors que l'action clone est encleché
             // Pas de traitement de l'action clone : remplacé par le traitement de l'action multiclone
             if ($action === 'clone') {
